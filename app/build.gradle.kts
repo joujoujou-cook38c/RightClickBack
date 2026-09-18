d6448da5cc76e438df322282b162a95059d0220f
joujoujou-cook38c <127681990+joujoujou-cook38c@users.noreply.github.com>
2026-09-18 17:40:32 +0800

    Initialize build.gradle.kts for Android project

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.rightclickback"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.rightclickback"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // 純 Android 原生零依賴，保證 100% 編譯成功
+}
